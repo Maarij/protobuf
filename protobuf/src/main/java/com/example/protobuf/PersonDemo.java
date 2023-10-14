ec7e145196f1cae851905079fd77a3098c8fe06e
Maarij <maarij.anwar@gmail.com>
2023-10-14 14:05:49 -0500

    serialization & deserialization

--- protobuf/src/main/java/com/example/protobuf/PersonDemo.java
+++ protobuf/src/main/java/com/example/protobuf/PersonDemo.java
@@ -2,14 +2,25 @@ package com.example.protobuf;
 
 import com.example.models.Person;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class PersonDemo {
 
-    public static void main(String[] args) {
-        Person sam = Person.newBuilder()
-                .setName("sam")
-                .setAge(10)
-                .build();
+    public static void main(String[] args) throws IOException {
+//        Person sam = Person.newBuilder()
+//                .setName("sam")
+//                .setAge(10)
+//                .build();
 
-        System.out.println(sam);
+        Path path = Paths.get("sam.ser");
+//        Files.write(path, sam.toByteArray());
+
+        byte[] bytes = Files.readAllBytes(path);
+        Person newSam = Person.parseFrom(bytes);
+
+        System.out.println(newSam);
     }
 }
