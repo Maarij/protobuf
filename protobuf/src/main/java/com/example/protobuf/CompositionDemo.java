2486aeb2c774d37466313306f593206d088777e2
Maarij <maarij.anwar@gmail.com>
2023-10-15 15:32:23 -0500

    collection demo

--- protobuf/src/main/java/com/example/protobuf/CompositionDemo.java
+++ protobuf/src/main/java/com/example/protobuf/CompositionDemo.java
@@ -4,6 +4,8 @@ import com.example.models.Address;
 import com.example.models.Car;
 import com.example.models.Person;
 
+import java.util.List;
+
 public class CompositionDemo {
 
     public static void main(String[] args) {
@@ -13,16 +15,24 @@ public class CompositionDemo {
                 .setCity("Atlanta")
                 .build();
 
-        Car car = Car.newBuilder()
+        Car accord = Car.newBuilder()
                 .setMake("Honda")
                 .setModel("Accord")
                 .setYear(2020)
                 .build();
 
+        Car civic = Car.newBuilder()
+                .setMake("Honda")
+                .setModel("Civic")
+                .setYear(2020)
+                .build();
+
+        List<Car> cars = List.of(accord, civic);
+
         Person sam = Person.newBuilder()
                 .setName("sam")
                 .setAge(25)
-                .setCar(car)
+                .addAllCar(cars)
                 .setAddress(address)
                 .build();
 
