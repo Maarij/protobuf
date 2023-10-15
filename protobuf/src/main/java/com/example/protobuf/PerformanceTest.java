4274110dde20ac0d9b28bf6c55acb1bfaf069ba5
Maarij <maarij.anwar@gmail.com>
2023-10-15 09:33:34 -0500

    json vs protobuf comparison test

--- protobuf/src/main/java/com/example/protobuf/PerformanceTest.java
+++ protobuf/src/main/java/com/example/protobuf/PerformanceTest.java
@@ -1,21 +1,67 @@
 package com.example.protobuf;
 
+import com.example.json.JPerson;
+import com.example.models.Person;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class PerformanceTest {
 
     public static void main(String[] args) {
 
         // json
-        Runnable runnable1;
+        JPerson person = new JPerson();
+        person.setName("sam");
+        person.setAge(10);
+        ObjectMapper mapper = new ObjectMapper();
+        Runnable json = () -> {
+            try {
+                byte[] bytes = mapper.writeValueAsBytes(person);
+                JPerson person1 = mapper.readValue(bytes, JPerson.class);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        };
 
         // protobuf
-        Runnable runnable2;
+        Person sam = Person.newBuilder()
+                .setName("sam")
+                .setAge(10)
+                .build();
+        Runnable protobuf = () -> {
+            try {
+                byte[] bytes = sam.toByteArray();
+                Person sam1 = Person.parseFrom(bytes);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        };
 
+        for (int i = 0; i < 5; i++) {
+            runPerformanceTest(json, "json");
+            runPerformanceTest(protobuf, "proto");
+        }
 
+        /*
+            json 2357ms
+            proto 331ms
+
+            json 1961ms
+            proto 271ms
+
+            json 1950ms
+            proto 269ms
+
+            json 1953ms
+            proto 266ms
+
+            json 1960ms
+            proto 268ms
+        */
     }
 
     private static void runPerformanceTest(Runnable runnable, String method) {
         long time1 = System.currentTimeMillis();
-        for (int i = 0; i < 1000000; i++) {
+        for (int i = 0; i < 5000000; i++) {
             runnable.run();
         }
         long time2 = System.currentTimeMillis();
