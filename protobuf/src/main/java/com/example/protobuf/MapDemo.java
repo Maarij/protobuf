eb4651fae2ef983d5021a46c5ef23a8c14ad6f2a
Maarij <maarij.anwar@gmail.com>
2023-10-15 15:35:50 -0500

    map demo

---
+++ protobuf/src/main/java/com/example/protobuf/MapDemo.java
@@ -0,0 +1,30 @@
+package com.example.protobuf;
+
+import com.example.models.Car;
+import com.example.models.Dealer;
+
+public class MapDemo {
+
+    public static void main(String[] args) {
+        Car accord = Car.newBuilder()
+                .setMake("Honda")
+                .setModel("Accord")
+                .setYear(2020)
+                .build();
+
+        Car civic = Car.newBuilder()
+                .setMake("Honda")
+                .setModel("Civic")
+                .setYear(2005)
+                .build();
+
+        Dealer dealer = Dealer.newBuilder()
+                .putModel(2005, civic)
+                .putModel(2020, accord)
+                .build();
+
+        System.out.println(dealer.getModelOrDefault(2019, accord));
+        System.out.println(dealer.getModelMap());
+
+    }
+}
