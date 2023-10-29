08dc291fc2be099df9a611177c32998b7545818d
Maarij <maarij.anwar@gmail.com>
2023-10-28 19:01:46 -0500

    snakes-ladder - die streaming observer implementation

---
+++ snakes-ladders/src/main/java/org/example/game/server/GameService.java
@@ -0,0 +1,18 @@
+package org.example.game.server;
+
+import com.example.game.Die;
+import com.example.game.GameServiceGrpc;
+import com.example.game.GameState;
+import com.example.game.Player;
+import io.grpc.stub.StreamObserver;
+
+public class GameService extends GameServiceGrpc.GameServiceImplBase {
+
+    @Override
+    public StreamObserver<Die> roll(StreamObserver<GameState> responseObserver) {
+        Player client = Player.newBuilder().setName("client").setPosition(0).build();
+        Player server = Player.newBuilder().setName("client").setPosition(0).build();
+
+        return new DieStreamingRequest(client, server, responseObserver);
+    }
+}
