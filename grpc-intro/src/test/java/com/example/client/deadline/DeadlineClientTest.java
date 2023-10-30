6e12d83524c8fdfa34b4f74ee10bc10d66dbf840
Maarij <maarij.anwar@gmail.com>
2023-10-30 10:03:33 -0500

    deadline - project setup

---
+++ grpc-intro/src/test/java/com/example/client/deadline/DeadlineClientTest.java
@@ -0,0 +1,60 @@
+package com.example.client.deadline;
+
+import com.example.client.rpctypes.BalanceStreamObserver;
+import com.example.client.rpctypes.MoneyStreamingResponse;
+import com.example.models.*;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.concurrent.CountDownLatch;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class DeadlineClientTest {
+
+    private BankServiceGrpc.BankServiceBlockingStub blockingStub;
+    private BankServiceGrpc.BankServiceStub bankServiceStub;
+
+    @BeforeAll
+    public void setup() {
+        ManagedChannel managedChannel = ManagedChannelBuilder.forAddress("localhost", 6565)
+                .usePlaintext()
+                .build();
+
+        this.blockingStub = BankServiceGrpc.newBlockingStub(managedChannel);
+        this.bankServiceStub = BankServiceGrpc.newStub(managedChannel);
+    }
+
+    @Test
+    public void balanceTest() throws InterruptedException {
+        Thread.sleep(5000);
+        BalanceCheckRequest balanceRequest = BalanceCheckRequest.newBuilder()
+                .setAccountNumber(7)
+                .build();
+
+        // Send request and wait for response (synchronous)
+        Balance balance = this.blockingStub.getBalance(balanceRequest);
+
+        System.out.println("Received: " + balance.getAmount());
+    }
+
+    @Test
+    public void withdrawTest() {
+        WithdrawRequest withdrawRequest = WithdrawRequest.newBuilder().setAccountNumber(7).setAmount(40).build();
+        this.blockingStub.withdraw(withdrawRequest)
+                .forEachRemaining(money -> System.out.println("Received: " + money.getValue()));
+    }
+
+    @Test
+    public void withdrawAsyncTest() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
+
+        WithdrawRequest withdrawRequest = WithdrawRequest.newBuilder().setAccountNumber(7).setAmount(40).build();
+        this.bankServiceStub.withdraw(withdrawRequest, new MoneyStreamingResponse(latch));
+        latch.await();
+    }
+
+}
